107f15fc24d771f50c69f88420c5b196388c98f2
kouphasi <89081817+kouphasi@users.noreply.github.com>
2026-01-30 00:54:01 +0900

    Fix Vue 3 test compiler resolution

--- vite.v3.config.ts
+++ vite.v3.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import * as compiler from "@vue/compiler-sfc-vue3";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({ compiler })],
   resolve: {
     alias: {
       vue: "vue3",
